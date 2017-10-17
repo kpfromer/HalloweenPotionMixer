a8718a9d9954f397c2feadc9feae1f29c1d08577
Kyle Pfromer <kpfromer2@gmail.com>
2017-10-17 08:33:11 -0600

    Add Ingredient model

--- src/app/model/ingredient.ts
+++ src/app/model/ingredient.ts
@@ -1,2 +1,10 @@
 export class Ingredient {
+  name: string;
+  imageUrl: string;
+
+
+  constructor(details: { name: string, imageUrl: string }) {
+    this.name = details.name;
+    this.imageUrl = details.imageUrl;
+  }
 }
