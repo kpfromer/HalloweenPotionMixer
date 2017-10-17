5526c4e0120302249011802e73d74bc1519f38be
Kyle Pfromer <kpfromer2@gmail.com>
2017-10-17 08:33:21 -0600

    Add Potion model

--- src/app/model/potion.ts
+++ src/app/model/potion.ts
@@ -1,2 +1,14 @@
+import {Ingredient} from './ingredient';
+
 export class Potion {
+  name: string;
+  imageUrl: string;
+  ingredients: Ingredient[];
+
+
+  constructor(data: { name: string, imageUrl: string, ingredients: Ingredient[] }) {
+    this.name = data.name;
+    this.imageUrl = data.imageUrl;
+    this.ingredients = data.ingredients;
+  }
 }
