f368b0ebcb7e6d854b91c7e0f9f5d851f1ee1a07
Kyle Pfromer <kpfromer2@gmail.com>
2017-10-17 08:27:34 -0600

    Add drag and drop to CauldronListComponent

--- src/app/cauldron-list/cauldron-list.component.ts
+++ src/app/cauldron-list/cauldron-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../model/ingredient';
+import {DragulaService} from 'ng2-dragula';
+import {isEqual, sortBy} from 'lodash';
+import {Potion} from '../model/potion';
+import {LocalStorage} from 'ngx-store';
+import {NewPotionService} from '../shared/new-potion.service';
 
 @Component({
   selector: 'app-cauldron-list',
@@ -7,7 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CauldronListComponent implements OnInit {
 
-  constructor() { }
+  @LocalStorage('potionRecipes') potionRecipes: Array<Potion> = [];
+
+  @LocalStorage('cauldronIngredients') ingredients: Array<Ingredient> = [];
+
+  constructor(dragulaService: DragulaService, private potionService: NewPotionService) {
+    dragulaService.dropModel.subscribe(() => this.onDrop());
+    dragulaService.removeModel.subscribe(() => this.onDrop());
+  }
+
+  onDrop() {
+    for (const potionRecipe of this.potionRecipes)
+      if (isEqual(sortBy(potionRecipe.ingredients, 'name'), sortBy(this.ingredients, 'name')))
+        this.potionService.createNewPotion(potionRecipe);
+  }
 
   ngOnInit() {
   }
