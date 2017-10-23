b23275ecdba98919f115f66d8386e562a52b7738
Kyle Pfromer <kpfromer2@gmail.com>
2017-10-23 11:16:31 -0600

    Update CauldronListComponent limit to Two Potions
    Now it will create a mundane potion if there is no potionRecipe.

--- src/app/cauldron-list/cauldron-list.component.ts
+++ src/app/cauldron-list/cauldron-list.component.ts
@@ -5,6 +5,7 @@ import {isEqual, sortBy} from 'lodash';
 import {Potion} from '../model/potion';
 import {LocalStorage} from 'ngx-store';
 import {NewPotionService} from '../shared/new-potion.service';
+import {MundanePotionService} from "../shared/mundane-potion.service";
 
 @Component({
   selector: 'app-cauldron-list',
@@ -17,15 +18,24 @@ export class CauldronListComponent implements OnInit {
 
   @LocalStorage('cauldronIngredients') ingredients: Array<Ingredient> = [];
 
-  constructor(dragulaService: DragulaService, private potionService: NewPotionService) {
+  constructor(dragulaService: DragulaService, private potionService: NewPotionService, private mundanePotion: MundanePotionService) {
     dragulaService.dropModel.subscribe(() => this.onDrop());
     dragulaService.removeModel.subscribe(() => this.onDrop());
   }
 
   onDrop() {
-    for (const potionRecipe of this.potionRecipes)
-      if (isEqual(sortBy(potionRecipe.ingredients, 'name'), sortBy(this.ingredients, 'name')))
-        this.potionService.createNewPotion(potionRecipe);
+    if (this.ingredients.length !== 2) {
+      return false;
+    } else if (this.ingredients.length >= 2) {
+      for (const potionRecipe of this.potionRecipes) {
+        if (isEqual(sortBy(potionRecipe.ingredients, 'name'), sortBy(this.ingredients, 'name'))) {
+          this.potionService.createNewPotion(potionRecipe);
+          return this.ingredients = [];
+        }
+      }
+      this.potionService.createNewPotion(this.mundanePotion.potion);
+      return this.ingredients = [];
+    }
   }
 
   ngOnInit() {
