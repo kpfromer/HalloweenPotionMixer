81f22fd396a0954c3fc46429ea4e641135f22c5c
Kyle Pfromer <kpfromer2@gmail.com>
2017-10-23 20:18:46 -0600

    Update PotionListComponent trigger effect on click
    Now potions will have an effect triggered on clicks.

--- src/app/potion-list/potion-list.component.ts
+++ src/app/potion-list/potion-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {LocalStorage} from 'ngx-store';
 import {NewPotionService} from '../shared/new-potion.service';
 import {Potion} from '../model/potion';
+import {RecipesService} from "../shared/recipes.service";
+import {MundanePotionService} from "../shared/mundane-potion.service";
 
 @Component({
   selector: 'app-potion-list',
@@ -12,7 +14,15 @@ export class PotionListComponent implements OnInit {
 
   @LocalStorage() potions: Potion[] = [];
 
-  constructor(private potionService: NewPotionService) {
+  click(potion: Potion) {
+    const realPotion = this.recipeService.potionRecipes.find(potionItem => potionItem.name === potion.name);
+    if (realPotion)
+      realPotion.effect();
+    else
+      this.mundanePotion.potion.effect();
+  }
+
+  constructor(private potionService: NewPotionService, private recipeService: RecipesService, private mundanePotion: MundanePotionService) {
     this.potionService.newPotion.subscribe(newPotion => this.potions.push(newPotion));
   }
 
