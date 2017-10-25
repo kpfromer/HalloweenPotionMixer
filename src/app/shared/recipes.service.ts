86d4eef25cc336770d4f0d05aad441d729b21fb2
Kyle Pfromer <kpfromer2@gmail.com>
2017-10-25 14:54:08 -0600

    Update RecipesService lists

--- src/app/shared/recipes.service.ts
+++ src/app/shared/recipes.service.ts
@@ -8,39 +8,375 @@ export class RecipesService {
 
   ingredientList: IngredientList = new IngredientList([
     new Ingredient({
-      name: 'AP Test',
-      imageUrl: 'https://www.mndhs.org/uploaded/academics/guidance/apcollegeboardlogo2.png'
+      name: 'Standardized Tests', imageUrl: ''
     }),
     new Ingredient({
-      name: 'IB Test',
-      imageUrl: 'http://www.eduinreview.com/blog/wp-content/uploads/2009/03/international-baccalaureate.gif'
+      name: 'College', imageUrl: ''
     }),
     new Ingredient({
-      name: 'Pencil',
-      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61N7NOKhVCL._SX355_.jpg'
+      name: 'Phone', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'Computer', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'A Panther', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'A Knight', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'BVSD', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'Cookies', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'Football', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'Pumpkin', imageUrl: ''
+    }),
+    new Ingredient({
+      name: 'fairviewhs.org', imageUrl: ''
     })
   ]);
 
   potionRecipes: Array<Potion> = [
+    // Standardized Tests
     new Potion({
-      name: 'Stress',
+      name: 'Stress Explosion',
       imageUrl: 'http://www.helpguide.org/images/stress/stressed-man-500.jpg',
       ingredientList: this.ingredientList,
       ingredients: [
-        'AP Test',
-        'IB Test'
+        'Standardized Tests',
+        'College'
       ],
-      effect: () => console.log('yo')
+      effect: () => {
+      } // todo: shake screen
     }),
     new Potion({
-      name: 'IB Diploma',
-      imageUrl: 'https://cdn2.hubspot.net/hubfs/360031/body_ibcurintro.jpg',
+      name: 'Laziness',
+      imageUrl: '',
       ingredientList: this.ingredientList,
       ingredients: [
-        'Pencil',
-        'IB Test'
+        'Standardized Tests',
+        'Phone'
       ],
-      effect: () => console.log('test')
+      effect: () => window.location.href = 'http://www.coolmath-games.com/0-run-3'
+    }),
+    new Potion({
+      name: 'Procrastination',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Standardized Tests',
+        'Cookies'
+      ],
+      effect: () => {
+      }// todo: ?
+    }),
+    new Potion({
+      name: 'Money',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Standardized Tests',
+        'BVSD'
+      ],
+      effect: () => {
+      } // todo: rain down dollar bills?
+    }),
+    new Potion({
+      name: 'Procrastination',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Standardized Tests',
+        'Football'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Honors',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Standardized Tests',
+        'fairviewhs.org'
+      ],
+      effect: () => window.location.href = 'https://www.fairviewhs.org/honors'
+    }),
+
+    // College
+    new Potion({
+      name: 'Insomnia',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'College',
+        'A Panther'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Stress',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'College',
+        'A Knight'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Stress',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'College',
+        'Cookies'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Concussion',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'College',
+        'Football'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+
+    // Phone
+
+    new Potion({
+      name: 'Happiness', // todo: puppy explosion
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Phone',
+        'Computer'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'BVSD Error',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Phone',
+        'BVSD'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Web Cookie',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Phone',
+        'Cookies'
+      ],
+      effect: () => window.location.href =
+        'http://www.makeuseof.com/tag/whats-a-cookie-and-what-does-it-have-to-do-with-my-privacy-makeuseof-explains/'
+    }),
+    new Potion({
+      name: 'Pinterest Board',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Phone',
+        'Pumpkin'
+      ],
+      effect: () => window.location.href = 'https://www.pinterest.com/explore/jack-o\'-lantern/?lp=true'
+    }),
+    new Potion({
+      name: 'Mobile Site',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Phone',
+        'fairviewhs.org'
+      ],
+      effect: () => window.location.href = 'https://www.fairviewhs.org/?mobile=1'
+    }),
+
+    // A Knight
+
+    new Potion({
+      name: 'Fairview Web Team',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Knight',
+        'Computer'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Rivalry',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Knight',
+        'A Panther'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'BVSD Error',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Knight',
+        'BVSD'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'IB Store',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Knight',
+        'Cookies'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Rivalry',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Knight',
+        'Football'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Trick or Treat Street',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Knight',
+        'Pumpkin'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Fairview Web Team',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Knight',
+        'fairviewhs.org'
+      ],
+      effect: () => window.location.href = 'https://www.fairviewhs.org/'
+    }),
+
+    // A Panther
+
+    new Potion({
+      name: 'BVSD Error',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Panther',
+        'BVSD'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'Rivalry',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Panther',
+        'Football'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'mr skeltal',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Panther',
+        'Pumpkin'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+    new Potion({
+      name: 'April Fools',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'A Panther',
+        'fairviewhs.org'
+      ],
+      effect: () => {
+      } // todo: ?
+    }),
+
+    // Cookie
+
+    new Potion({
+      name: 'Web Cookie',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'Cookie',
+        'fairviewhs.org'
+      ],
+      effect: () =>
+        window.location.href = 'http://www.makeuseof.com/tag/whats-a-cookie-and-what-does-it-have-to-do-with-my-privacy-makeuseof-explains/'
+    }),
+
+    // BVSD
+
+    new Potion({
+      name: 'mr skeltal',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'BVSD',
+        'Pumpkin'
+      ],
+      effect: () => {
+      } // todo: add
+    }),
+
+    // Pumpkin
+
+    new Potion({
+      name: 'mr skeltal',
+      imageUrl: '',
+      ingredientList: this.ingredientList,
+      ingredients: [
+        'fairviewhs.org',
+        'Pumpkin'
+      ],
+      effect: () => {
+      } // todo: add
     })
   ];
 
